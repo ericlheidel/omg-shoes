54543d18038561efe494c5cfa22cc15bca091a79
Eric Heidel <ericleeheidel@yahoo.com>
2024-02-17 17:20:53 -0600

    Ready to Demo

--- src/welcome/Welcome.js
+++ src/welcome/Welcome.js
@@ -7,7 +7,7 @@ export const Welcome = () => {
         <span>Welcome to</span>
         <span>OMG, Shoes...</span>
       </h1>
-      <div>Your Home for Vintage Dunks</div>
+      <div>Your Home for Vintage Nike Dunks</div>
       <img
         src="https://pbs.twimg.com/media/E25w2ORWQAMuBio?format=jpg&name=medium"
         alt="Pile of Nike dunks"
